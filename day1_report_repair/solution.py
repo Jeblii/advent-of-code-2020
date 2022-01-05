79efd4aa7565e7ee1cf36e618cb265207ecf4b06
Jeffrey Tsang <jeffreytsang@Jeffreys-MacBook-Pro.local>
2022-01-05 14:28:55 +0100

    completed day one

---
+++ day1_report_repair/solution.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+with open ('day1_report_repair/input.txt') as f:
+    lines = f.read().splitlines()
+
+lines = [int(line) for line in lines]
+
+# part one
+for entries in list(combinations(lines, 2)):
+    n1, n2 = entries[0], entries[1]
+    if n1 + n2 == 2020:
+        print(n1, n2)
+        print(n1 * n2)
+        break
+
+# part two
+for entries in list(combinations(lines, 3)):
+    n1, n2, n3 = entries[0], entries[1], entries[2]
+    if n1 + n2 + n3 == 2020:
+        print(n1, n2, n3)
+        print(n1 * n2 * n3)
+        break
