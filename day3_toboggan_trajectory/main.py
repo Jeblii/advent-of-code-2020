50b5e63b722776c67f225896c7f88c0981c5765d
Jeffrey Tsang <jeffreytsang@Jeffreys-MacBook-Pro.local>
2022-01-28 10:09:37 +0100

    part two completed day 3 toboggan trajectory

--- day3_toboggan_trajectory/main.py
+++ day3_toboggan_trajectory/main.py
@@ -1,13 +1,14 @@
 from typing import List
+import numpy as np
 
 with open("day3_toboggan_trajectory/input.txt") as f:
     lines = f.read().splitlines()
 
-def part_one(map: List[List[str]], h_move: int, v_move: int) -> int:
+def traverse_map(map: List[List[str]], h_move: int, v_move: int) -> int:
     x = 0
     y = 0
     trees_encountered = 0
-    while y != len(map): # if your position is at the last row of the map, stop
+    while True: # if your position is at the last row of the map, stop
         x = x % len(map[0]) # instead of 'copying' the map to the right, I use modulo to traverse the same map instead
         if map[y][x] == '#':
             trees_encountered += 1
@@ -15,6 +16,16 @@ def part_one(map: List[List[str]], h_move: int, v_move: int) -> int:
         x += h_move
         y += v_move
 
+        if y >= len(map):
+            break
+
     return trees_encountered
 
-print(part_one(lines, 3, 1))
+#print(part_one(lines, 3, 1))
+
+# part two
+options = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+answers = [traverse_map(lines, option[0], option[1]) for option in options]
+result = np.prod(np.array(answers))  
+
+print(result)
