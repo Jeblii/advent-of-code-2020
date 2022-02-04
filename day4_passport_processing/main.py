e54885b767fe7af606666f226a776f9e8df6475f
Jeffrey Tsang <jeffreytsang@Jeffreys-MacBook-Pro.local>
2022-02-04 12:05:01 +0100

    parsing code for aprt 1 day 4

--- day4_passport_processing/main.py
+++ day4_passport_processing/main.py
@@ -1,14 +1,28 @@
 from typing import List
-with open("day4_passport_processing/example_input.txt") as f:
-    lines = f.read().splitlines()
+import dataclasses as dc
 
-def parse_keys(lines:List[List[str]]) -> List[dict]:
+required_keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+def is_valid(keys: List[str]) -> bool:
+    # cid is not required
+    intersection_keys = set(keys).intersection(set(required_keys))
+    if intersection_keys == set(required_keys):
+        return True
+    return False
+
+def parse_keys(lines:List[List[str]]) -> List[str]:
     keys = []
-    for line in lines:
-        splitted_line = line.split()
-        for l in splitted_line:
-            k, _ = l.split(':')
-            keys.append(k)
+    splitted_line = lines.split()
+    for l in splitted_line:
+        k, _ = l.split(':')
+        keys.append(k)
+    return keys
+
+with open("day4_passport_processing/example_input.txt") as f:
+    lines = f.read()
+
+passport_data = lines.split('\n\n')
+for passport in passport_data:
+    print(is_valid(parse_keys(passport)))
 
 
-print(lines)
